d1a384e73ed5dc7167c8b4e514d7500ff3df0943
Jeremy Mesinas <jeremymesinas22@gmail.com>
2025-10-04 10:26:30 +0800

    Page 55 checkpoint

--- controller/product.php
+++ controller/product.php
@@ -38,7 +38,114 @@ if (array_key_exists("productid", $_GET)) {
     }
 }
 
-if ($_SERVER)
+if ($_SERVER['REQUEST_METHOD']==='GET') {
+    //query db
 
+    try {
+        $query = $readDB->prepare('SELECT product_id, product_name, description, DATE_FORMAT(delivered_date, "%d/%m/%Y %H:%i") as delivered_date from tblproducts where product_id = :productid');
+        $query->bindParam(':productid', $productid, PDO::PARAM_INT);
+        $query->execute();
+
+        // get row count
+        $rowCount = $query->rowCount();
+        // create product array to store returned product
+        $productArray = array();
+
+        if($rowCount === 0) {
+        // set up response for unsuccessful return
+        $response = new Response();
+        $response->setHttpStatusCode(404);
+        $response->setSuccess(false);
+        $response->addMessage("Product not found");
+        $response->send();
+        exit;
+        }
+
+        // for each row returned
+        while($row = $query->fetch(PDO::FETCH_ASSOC)) {
+            // create new product object for each row
+            $product = new Product($row['product_id'], $row['product_name'], $row['description'], $row['delivered_date']);
+
+            // create product and store in array for return in json data
+            $productArray[] = $product->returnProductAsArray();
+        }
+
+        // bundle products and rows returned into an array to return in the json data
+        $returnData = array();
+        $returnData['rows_returned'] = $rowCount;
+        $returnData['products'] = $productArray;
+
+        // set up response for successful return
+        $response = new Response();
+        $response->setHttpStatusCode(200);
+        $response->setSuccess(true);
+        $response->toCache(true);
+        $response->setData($returnData);
+        $response->send();
+        exit;
+    }
+
+    // if error with sql query return a json error
+    catch(ProductException $ex) {
+        $response = new Response();
+        $response->setHttpStatusCode(500);
+        $response->setSuccess(false);
+        $response->addMessage($ex->getMessage());
+        $response->send();
+        exit;
+    }
+    catch(PDOException $ex) {
+        error_log("Database Query Error: ".$ex, 0);
+        $response = new Response();
+        $response->setHttpStatusCode(500);
+        $response->setSuccess(false);
+        $response->addMessage("Failed to get product");
+        $response->send();
+        exit;
+    }
+}
+
+// else if request if a DELETE e.g. delete product
+elseif($_SERVER['REQUEST_METHOD'] === 'DELETE') {
+    // attempt to query the database
+    try {
+        // create db query
+        $query = $writeDB->prepare('delete from tblproducts where product_id = :productid');
+        $query->bindParam(':productid', $productid, PDO::PARAM_INT);
+        $query->execute();
+
+        // get row count
+        $rowCount = $query->rowCount();
+
+        if($rowCount === 0) {
+            // set up response for unsuccessful return
+            $response = new Response();
+            $response->setHttpStatusCode(404);
+            $response->setSuccess(false);
+            $response->addMessage("Product not found");
+            $response->send();
+            exit;
+        }
+    }
+
+    // set up response for successful return
+    $response = new Response();
+    $response->setHttpStatusCode(200);
+    $response->setSuccess(true);
+    $response->addMessage("Product deleted");
+    $response->send();
+    exit;
+    
+
+    // if error with sql query return a json error
+    catch(PDOException $ex) {
+        $response = new Response();
+        $response->setHttpStatusCode(500);
+        $response->setSuccess(false);
+        $response->addMessage("Failed to delete product");
+        $response->send();
+        exit;
+    }
+}
 
 ?>
